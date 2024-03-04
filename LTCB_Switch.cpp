a1339597112138f310211bc021763cf0ab1880b8
ADMIN <ADMIN@LAB1-MAY03>
2024-03-04 08:09:36 +0700

    Add project files.

---
+++ LTCB_Switch.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int day = 4;
+	switch (day) {
+	case 1:
+		cout << "Monday";
+		break;
+	case 2:
+		cout << "tuseday";
+		break;
+	case 3:
+		cout << "wednesday";
+		break;
+	case 4:
+		cout << "thurday";
+		break;
+	case 5:
+		cout << "friday";
+		break;
+	case 6:
+		cout << "saturday";
+		break;
+	case 7:
+		cout << "sunday";
+		break;
+	}
+}
+
